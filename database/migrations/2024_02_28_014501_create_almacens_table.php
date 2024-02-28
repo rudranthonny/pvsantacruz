56d268d1985d5adeddaeebfda4d3cf5b65dd9cd4
Alexis-chata <alexizz.19.ac@gmail.com>
2024-02-27 20:50:44 -0500

    Avance

---
+++ database/migrations/2024_02_28_014501_create_almacens_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('almacens', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('telefono');
+            $table->string('pais');
+            $table->string('cuidad');
+            $table->string('email');
+            $table->string('codigo_postal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('almacens');
+    }
+};
