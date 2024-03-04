ae7d05e584d92364acf7df4461453bf42ffcad92
edinsondc <edinsoncd@outlook.com>
2024-03-03 22:19:07 -0500

    Actualizado

---
+++ resources/views/administrador/ajustes/marcas.blade.php
@@ -0,0 +1,26 @@
+@extends('adminlte::page')
+
+@section('title', 'Marcas')
+
+@section('content_header')
+    <h1></h1>
+@stop
+
+@section('content')
+    @livewire('gestionar-marcas')
+@stop
+
+@section('css')
+    <link href="{{ asset('css/css_bootstrap.min.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/sweetalert2@11.js') }}"></script>
+@stop
+
+@section('js')
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/sweetalert2@11.js') }}"></script>
+    <script>
+         Livewire.on('cerrar_modal_moneda', reservacion => {
+            ventana = document.getElementById('cerrar_modal_moneda_x').click();
+        });
+    </script>
+@stop
--- routes/admin.php
+++ routes/admin.php
@@ -5,3 +5,4 @@ Route::view('', 'administrador.index')->name('admin.index');
 Route::view('configuracion/ajustes_sistema', 'administrador.configuracion.ajustesistema')->name('admin.configuracion.ajustesistema');
 Route::view("moneda","administrador.ajustes.moneda")->name("moneda");
 Route::view("almacen","administrador.ajustes.almacen")->name("admin.almacen");
+Route::view("marcas","administrador.ajustes.marcas")->name("admin.marcas");
