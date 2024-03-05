f855819f4b91b204118beecf899ca029e3f62da2
rudranthonny <ing.anthonny.joel@gmail.com>
2024-03-05 15:46:40 -0500

    actualizar

--- routes/admin.php
+++ routes/admin.php
@@ -5,5 +5,7 @@ Route::view('', 'administrador.index')->name('admin.index');
 Route::view('configuracion/ajustes_sistema', 'administrador.configuracion.ajustesistema')->name('admin.configuracion.ajustesistema');
 Route::view("moneda","administrador.ajustes.moneda")->name("moneda");
 Route::view("almacen","administrador.ajustes.almacen")->name("admin.almacen");
+#productos
 Route::view("productos","administrador.productos.productos")->name("admin.productos");
 Route::view("marcas","administrador.productos.marcas")->name("admin.marcas");
+Route::view("codigo_barra","administrador.productos.codigo_barra")->name("admin.codigo_barra");
