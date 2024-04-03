904cb0470d6d0d7eac93e354ec386077334c5f97
rudranthonny <ing.anthonny.joel@gmail.com>
2024-04-02 19:54:17 -0500

    acttualizar

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -20,6 +20,10 @@ class RoleSeeder extends Seeder
 
         //Role::create(['name' => 'Administrador']);
        // Role::create(['name' => 'Cajero']);
+       $permisos = Permission::all();
+       foreach ($permisos as $key => $per) {
+        $per->delete();
+       }
         Permission::create(['name' =>'admin.configuracion.ajustesistema'])->syncRoles(['Administrador']);
         Permission::create(['name' =>'admin.index'])->syncRoles(['Administrador','Cajero']);
         Permission::create(['name' =>'admin.moneda'])->syncRoles(['Administrador']);
