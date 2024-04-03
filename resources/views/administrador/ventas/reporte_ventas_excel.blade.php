54a7796d1dcabe5112b18b536c448e72555c8e62
rudranthonny <ing.anthonny.joel@gmail.com>
2024-04-02 19:39:51 -0500

    actualizar

--- resources/views/administrador/ventas/reporte_ventas_excel.blade.php
+++ resources/views/administrador/ventas/reporte_ventas_excel.blade.php
@@ -1,41 +1,38 @@
 <table class="table table-hover">
     <thead class="table-light">
         <tr class="text-center">
-            <th>Fecha</th>
-            <th>Almacen</th>
-            <th>Cliente</th>
-            <th>Impuesto Porcentaje</th>
-            <th>Impuesto</th>
-            <th>Descuento</th>
-            <th>Envio</th>
-            <th>Total a Pagar</th>
-            <th>Acciones</th>
+            <th style="background-color: black;color:white;width:150px;text-align:center;">Fecha</th>
+            <th style="background-color: black;color:white;width:150px;text-align:center;">Almacen</th>
+            <th style="background-color: black;color:white;width:150px;text-align:center;">Cliente</th>
+            <th style="background-color: black;color:white;width:150px;text-align:center;">Impuesto Porcentaje</th>
+            <th style="background-color: black;color:white;width:150px;text-align:center;">Impuesto</th>
+            <th style="background-color: black;color:white;width:150px;text-align:center;">Descuento</th>
+            <th style="background-color: black;color:white;width:150px;text-align:center;">Envio</th>
+            <th style="background-color: black;color:white;width:150px;text-align:center;">Total a Pagar</th>
         </tr>
     </thead>
     <tbody>
-        @forelse ($posventas as $pventa)
+        @forelse ($lista_ventas as $pventa)
         <tr>
-            <td role="cell" class="text-center"  style="vertical-align: middle;">{{ $pventa->created_at }}</td>
-            <td role="cell" class="text-center" style="vertical-align: middle;">{{ $pventa->almacen_name }}</td>
-            <td role="cell" class="text-center" style="vertical-align: middle;">{{ $pventa->cliente_name }}</td>
-            <td role="cell" class="text-center" style="vertical-align: middle;">{{ $pventa->impuesto_porcentaje }}%</td>
-            <td role="cell" class="text-center" style="vertical-align: middle;">{{ $configuracion->moneda->simbolo.$pventa->impuesto_monto }}</td>
-            <td role="cell" class="text-center" style="vertical-align: middle;">{{ $configuracion->moneda->simbolo.$pventa->descuento }}</td>
-            <td role="cell" class="text-center" style="vertical-align: middle;">{{ $configuracion->moneda->simbolo.$pventa->envio }}</td>
-            <td role="cell" class="text-center" style="vertical-align: middle;">{{ $configuracion->moneda->simbolo.$pventa->total_pagar }}</td>
-            <td role="cell" class="text-center" style="vertical-align: middle;"><button class="btn btn-danger" wire:loading.attr='disabled' id="venta_descargar-{{$pventa->id}}" wire:target='descargar_venta_pdf({{$pventa->id}})' wire:click='descargar_venta_pdf({{$pventa->id}})'><i class="fas fa-download"></i></button></td>
+            <td style="border: solid 1px black;text-align: center;">{{ $pventa->created_at }}</td>
+            <td style="border: solid 1px black;text-align: center;">{{ $pventa->almacen_name }}</td>
+            <td style="border: solid 1px black;text-align: center;">{{ $pventa->cliente_name }}</td>
+            <td style="border: solid 1px black;text-align: center;">{{ $pventa->impuesto_porcentaje }}%</td>
+            <td style="border: solid 1px black;text-align: center;">{{ $configuracion->moneda->simbolo.$pventa->impuesto_monto }}</td>
+            <td style="border: solid 1px black;text-align: center;">{{ $configuracion->moneda->simbolo.$pventa->descuento }}</td>
+            <td style="border: solid 1px black;text-align: center;">{{ $configuracion->moneda->simbolo.$pventa->envio }}</td>
+            <td style="border: solid 1px black;text-align: center;">{{ $configuracion->moneda->simbolo.$pventa->total_pagar }}</td>
         </tr>
         @empty
         @endforelse
             <tr>
-                <td colspan="4" class="table-dark text-center">
+                <td colspan="4" style="background-color: black;color:white;text-align:center;">
                     Total
                 </td>
-                <td class="text-center table-success">{{$configuracion->moneda->simbolo.$posventas->sum('impuesto_monto')}}</td>
-                <td class="text-center table-success">{{$configuracion->moneda->simbolo.$posventas->sum('descuento')}}</td>
-                <td class="text-center table-success">{{$configuracion->moneda->simbolo.$posventas->sum('envio')}}</td>
-                <td class="text-center table-success">{{$configuracion->moneda->simbolo.$posventas->sum('total_pagar')}}</td>
-                <td class="text-center table-dark"></td>
+                <td class="text-center table-success" style="text-align: center;">{{$configuracion->moneda->simbolo.$lista_ventas->sum('impuesto_monto')}}</td>
+                <td class="text-center table-success" style="text-align: center;">{{$configuracion->moneda->simbolo.$lista_ventas->sum('descuento')}}</td>
+                <td class="text-center table-success" style="text-align: center;">{{$configuracion->moneda->simbolo.$lista_ventas->sum('envio')}}</td>
+                <td class="text-center table-success" style="text-align: center;">{{$configuracion->moneda->simbolo.$lista_ventas->sum('total_pagar')}}</td>
             </tr>
     </tbody>
 </table>
